81252cadb4ef5763a93595d1489202c95a570d2e
Jemimah000 <28dora.nobi@gmail.com>
2025-04-07 10:13:53 +0530

    updated

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const encrypt = (payload, secret) => {
-  // your code here and return token
+  const jwt = require('jsonwebtoken');
+
+const encrypt = (payload, secret) => {
+  const token = jwt.sign(payload, secret, { expiresIn: '1h' });
+  return token;
+};
+
+module.exports = encrypt;
+
 };
 
 module.exports = encrypt;
